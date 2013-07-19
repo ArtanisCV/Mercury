bb68fb2c3f9ca14849ecba03415d3b16f38fb658
Artanis <283186127@qq.com>
2013-07-19 14:36:23 +0800

    Add indexes

--- pci/ch04/main.py
+++ pci/ch04/main.py
@@ -2,6 +2,17 @@ import searchEngine
 
 __author__ = 'Artanis'
 
-pagelist = ['http://kiwitobes.com/wiki/Perl.html']
-crawler = searchEngine.crawler('')
-crawler.crawl(pagelist)
+# pagelist = ['http://kiwitobes.com/wiki/Perl.html']
+# crawler = searchEngine.crawler('')
+# crawler.crawl(pagelist)
+
+# crawler = searchEngine.crawler('SearchIndex.db')
+# crawler.createIndexTables()
+# pages = ['http://kiwitobes.com/wiki/Categorical_list_of_programming_languages.html']
+# crawler.crawl(pages)
+
+# result = [row for row in crawler.conn.execute("select urlId from WordLocation where wordId=1 order by urlId")]
+# print len(result), result
+
+result = searchEngine.Searcher('SearchIndex.db').getMatchRows("functional programming")[0]
+print len(result), result
--- pci/ch04/searchEngine.py
+++ pci/ch04/searchEngine.py
@@ -1,45 +1,97 @@
 import urllib2
 from BeautifulSoup import *
 from urlparse import urljoin
+from pysqlite2 import dbapi2 as sqlite
 
 __author__ = 'Artanis'
 
 
 # Create a list of words to ignore
-ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+ignoreWords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
 
 class crawler:
     # Initialize the crawler with the name of database
-    def __init__(self, dbname):
-        pass
+    def __init__(self, dbName):
+        self.conn = sqlite.connect(dbName)
 
     def __del__(self):
-        pass
+        self.conn.close()
 
     def dbCommit(self):
-        pass
+        self.conn.commit()
 
     # Auxilliary function for getting an entry id and adding
     # it if it's not present
     def getEntryId(self, table, field, value, createNew=True):
-        return None
+        handle = self.conn.execute("select rowId from %s where %s = '%s'" %
+                                  (table, field, value))
+        result = handle.fetchone()
+
+        if result is None:
+            if createNew:
+                handle = self.conn.execute("insert into %s (%s) values ('%s')" %
+                                          (table, field, value))
+                return handle.lastrowid
+            else:
+                return -1
+        else:
+            return result[0]
 
     # Index an individual page
     def addToIndex(self, url, soup):
+        if self.isIndexed(url):
+            return
+
         print 'Indexing %s' % url
+
+        # Get the individual words
+        text = self.getTextOnly(soup)
+        words = self.separateWords(text)
+
+        # Get the URL id
+        urlId = self.getEntryId('UrlList', 'url', url)
+
+        # Link each word to this url
+        for i in range(len(words)):
+            if words[i] not in ignoreWords:
+                wordId = self.getEntryId('WordList', 'word', words[i])
+
+                self.conn.execute("insert into WordLocation(urlId, wordId, location) values (%d, %d, %d)" %
+                                 (urlId, wordId, i))
 
     # Extract the text from an HTML page (no tags)
     def getTextOnly(self, soup):
-        return None
+        text = soup.string
+
+        if text is None:
+            text = ''
+
+            for content in soup.contents:
+                subText = self.getTextOnly(content)
+                text += subText + '\n'
+
+            return text
+        else:
+            return text.strip()
 
     # Separate the words by any non-whitespace character
     def separateWords(self, text):
-        return None
+        separators = re.compile('\\W*')
+        return [word.lower() for word in separators.split(text) if word != '']
 
     # Return true if this url is already indexed
     def isIndexed(self, url):
-        return False
+        urlResult = self.conn.execute("select rowId from UrlList where url = '%s'" % url).fetchone()
+
+        if urlResult is None:
+            return False
+        else:
+            # Check if it has actually been crawled
+            if self.conn.execute("select * from WordLocation where urlId = '%d'" % urlResult[0]).fetchone() is None:
+                return False
+            else:
+                return True
 
     # Add a link between two pages
     def addLinkRef(self, urlFrom, urlTo, linkText):
@@ -85,4 +137,56 @@ class crawler:
 
     # Create the database tables
     def createIndexTables(self):
-        pass
+        self.conn.execute('create table UrlList(url)')
+        self.conn.execute('create table WordList(word)')
+        self.conn.execute('create table WordLocation(urlId, wordId, location)')
+        self.conn.execute('create table Link(fromId integer, toId integer)')
+        self.conn.execute('create table LinkWords(wordId, linkId)')
+        self.conn.execute('create index WordIdx on WordList(word)')
+        self.conn.execute('create index UrlIdx on UrlList(url)')
+        self.conn.execute('create index WordUrlIdx on WordLocation(wordId)')
+        self.conn.execute('create index urlToIdx on Link(toId)')
+        self.conn.execute('create index urlFromIdx on Link(fromId)')
+        self.dbCommit()
+
+
+class Searcher:
+    def __init__(self, dbName):
+        self.conn = sqlite.connect(dbName)
+
+    def __del__(self):
+        self.conn.close()
+
+    def getMatchRows(self, query):
+        # Strings to build the database query
+        fieldList = 'wl0.urlId'
+        tableList = ''
+        clauseList = ''
+        tableIdx = 0
+        wordIds = []
+
+        # Split the words by spaces
+        for word in query.split(' '):
+            # Get the word ID
+            wordResult = self.conn.execute("select rowId from WordList where word = '%s'" % word).fetchone()
+
+            if wordResult is not None:
+                wordId = wordResult[0]
+                wordIds.append(wordId)
+
+                if tableIdx > 0:
+                    tableList += ', '
+                    clauseList += ' and wl%d.urlId = wl%d.urlId and ' % (tableIdx - 1, tableIdx)
+
+                fieldList += ', wl%d.location' % tableIdx
+                tableList += 'WordLocation wl%d' % tableIdx
+                clauseList += 'wl%d.wordId = %d' % (tableIdx, wordId)
+                tableIdx += 1
+
+        # Create the query from the separate parts
+        result = self.conn.execute("select %s from %s where %s" % (fieldList, tableList, clauseList))
+
+        if result is None:
+            return [], wordIds
+        else:
+            return [row for row in result], wordIds
